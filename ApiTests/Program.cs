35f20426575ed53f95007e971d97d036842dbfe7
Patrik Ahlén <patrik.zencoder@gmail.com>
2020-09-16 11:15:11 +0200

    consol added for lab

---
+++ ApiTests/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ApiTests
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+           
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                var response = await client.GetAsync("http://localhost:50150/api/appinfo");
+                response.EnsureSuccessStatusCode();
+                var responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
